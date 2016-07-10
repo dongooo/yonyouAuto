bfa68fa0ee3d627e0664102d76f2070e27158a7f
dongooo <dongooo@yahoo.com>
2016-07-10 22:44:20 +0800

    删除.idea

--- yyauto-web/src/main/java/com/yyauto/service/ICarModelService.java
+++ yyauto-web/src/main/java/com/yyauto/service/ICarModelService.java
@@ -4,4 +4,5 @@ package com.yyauto.service;
  * Created by dongooo on 16/7/8.
  */
 public interface ICarModelService {
+
 }
--- yyauto-web/src/main/java/com/yyauto/service/impl/CarModelServiceImpl.java
+++ yyauto-web/src/main/java/com/yyauto/service/impl/CarModelServiceImpl.java
@@ -1,7 +1,10 @@
 package com.yyauto.service.impl;
 
+import com.yyauto.service.ICarModelService;
+
 /**
  * Created by dongooo on 16/7/8.
  */
-public class CarModelServiceImpl {
+public class CarModelServiceImpl implements ICarModelService {
+
 }
